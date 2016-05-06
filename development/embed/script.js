05399b0b12f114083cec9f6877a9b488e337d7b8
Ire Aderinokun <ire@ireaderinokun.com>
2016-05-06 11:05:08 +0100

    test

--- development/embed/script.js
+++ development/embed/script.js
@@ -98,6 +98,8 @@ loadJSON(caniuseDataUrl, function(res) {
 			featureDescription = featureDescription.replace(/</g, "&lt;");
 			featureDescription = featureDescription.replace(/>/g, "&gt;");
 
+		console.log(featureDescription);
+
 		document.getElementById('featureTitle').innerHTML = feature.title;
 		document.getElementById('featureDescription').innerHTML = featureDescription;
 		document.getElementById('featureLink').href = 'http://caniuse.com/#feat=' + featureID;
