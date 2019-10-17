fa2fb75904a58137e4779464f7a13707509cdb7a
Ire Aderinokun <iaderinokun@gmail.com>
2019-10-17 10:53:31 -0700

    Add hello-world

--- functions/screenshot/screenshot.js
+++ functions/screenshot/screenshot.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.config({ 
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
