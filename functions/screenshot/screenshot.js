e35a9c466ff06302de74e95f08ceba729deb0034
Ire Aderinokun <iaderinokun@gmail.com>
2019-10-17 06:23:49 -0700

    Try: Move functions to single file

--- functions/screenshot/screenshot.js
+++ functions/screenshot/screenshot.js
@@ -1,5 +1,63 @@
-const takeScreenshot = require("./take-screenshot");
-const uploadScreenshot = require("./upload-screenshot");
+// const takeScreenshot = require("./take-screenshot");
+// const uploadScreenshot = require("./upload-screenshot");
+
+const puppeteer = require('puppeteer');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+async function takeScreenshot (feature, periods, accessibleColours) {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    defaultViewport: {
+      width: 800,
+      height: 500,
+      isLandscape: true
+    }
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(
+    `https://caniuse.bitsofco.de/embed/index.html?feat=${feature}&periods=${periods}&accessible-colours=${accessibleColours}`,
+    { waitUntil: 'networkidle2' }
+  );
+
+  const screenshot = await page.screenshot({
+    omitBackground: true,
+    encoding: 'binary'
+  });
+
+  await browser.close();
+
+  return screenshot;
+}
+
+function uploadScreenshot (feature, screenshot) {
+  return new Promise((resolve, reject) => {
+
+    const today = new Date();
+    const dd = today.getDate();
+    const mm = today.getMonth() + 1;
+    const yyyy = today.getFullYear();
+    const date = `${yyyy}-${mm}-${dd}`;
+
+    const options = {
+      folder: 'caniuse-embed',
+      public_id: `${feature}-${date}`
+    };
+
+    cloudinary.uploader.upload_stream(options, (error, result) => {
+      if (error) reject(error)
+      else resolve(result);
+    }).end(screenshot);
+  });
+}
+
 
 exports.handler = async (event, context) => {
 
